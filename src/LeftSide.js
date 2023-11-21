fa216212170160fdcf8725653cfc55493ea7548c
Hp <Hp@DESKTOP-ID5FAVQ>
2023-11-21 15:55:27 +0100

    c

--- src/LeftSide.js
+++ src/LeftSide.js
@@ -13,18 +13,12 @@ import {
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
-import './App.css'
+import "./App.css";
 export default function LeftSide() {
   return (
     <div className="container bg">
-      
       <div className="image-container text-center img-container">
-        <img
-          src={monImage}
-          alt=""
-         
-          className="profile-image img-thumbnail"
-        />
+        <img src={monImage} alt="" className="profile-image img-thumbnail" />
       </div>
       <h4>ABDELILAH KOUZIH</h4>
 
@@ -34,40 +28,36 @@ export default function LeftSide() {
           <span>Accueil</span>
         </Link>
 
-        <Link to="/compétences" >
+        <Link to="/compétences">
           <FontAwesomeIcon icon={faTasks} />
-           <span >Compétences</span>
+          <span>Compétences</span>
         </Link>
 
-        <Link to="/portfolio">
+        <Link to="https://abdelilahkouzih.github.io/Portfolio/">
           <FontAwesomeIcon icon={faBriefcase} />
-          <span >Portfolio</span>
+          <span>Portfolio</span>
         </Link>
 
         <Link to="/contact">
           <FontAwesomeIcon icon={faEnvelope} />
-          <span >Contact</span>
+          <span>Contact</span>
         </Link>
       </nav>
       <nav className="list-unstyled mt-6 nava">
-       
-         
-            <a href="https://www.linkedin.com/in/abdelilah-kouzih-957820245" className="text-dark heartbeat">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-         
-        
-            <a href="https://github.com/kozi11?tab=repositories" className="text-dark">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-       
+        <a
+          href="https://www.linkedin.com/in/abdelilah-kouzih-957820245"
+          className="text-dark heartbeat"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
 
-      
-            <a href="https://twitter.com/" className="text-dark">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-         
-        
+        <a href="https://github.com/AbdelilahKouzih" className="text-dark">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+
+        <a href="https://twitter.com/" className="text-dark">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
       </nav>
       <h5 className="text-center mt-4">Abdelilah-2023</h5>
     </div>
