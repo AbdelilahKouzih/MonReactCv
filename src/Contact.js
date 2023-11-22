b86b58e7ff868940f3789e2b8ec0ef41ce07bcf7
Hp <Hp@DESKTOP-ID5FAVQ>
2023-11-22 22:53:52 +0100

    commit3

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <form>
+        <div>
+          <img
+            src="https://cdn4.iconfinder.com/data/icons/basic-user-interface-elements/700/mail-letter-offer-256.png"
+            alt="icon"
+          />
+        </div>
+        <input type="text" placeholder="Nom" required />
+        <input type="email" placeholder="Adresse" required />
+        <input type="text" placeholder="Sujet" required />
+        <textarea placeholder="Description" required></textarea>
+        <input type="submit" value="Envoyer" />
+      </form>
     </div>
-  )
+  );
 }
